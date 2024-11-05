cb8b98b829cc96f53c4549ad59837bb146bb14ff
nicogorlo <gorlon@ethz.ch>
2024-11-05 14:48:40 -0500

    commented legacy code

--- scripts/test_environment_build.py
+++ scripts/test_environment_build.py
@@ -9,45 +9,44 @@ def main():
     iiwa1_model = plant.GetModelInstanceByName("iiwa_1")
     iiwa2_model = plant.GetModelInstanceByName("iiwa_2")
 
-    # Create controllers
-    controller1 = builder.AddSystem(DifferentialIKController(plant, iiwa1_model))
-    controller2 = builder.AddSystem(DifferentialIKController(plant, iiwa2_model))
+    # TODO Create controllers
+    # controller1 = builder.AddSystem(DifferentialIKController(plant, iiwa1_model))
+    # controller2 = builder.AddSystem(DifferentialIKController(plant, iiwa2_model))
 
-    # Connect state ports
-    builder.Connect(
-        plant.get_state_output_port(),
-        controller1.get_input_port(0)
-    )
-    builder.Connect(
-        controller1.get_output_port(0),
-        plant.get_actuation_input_port(iiwa1_model)
-    )
+    # builder.Connect(
+    #     plant.get_state_output_port(),
+    #     controller1.get_input_port(0)
+    # )
+    # builder.Connect(
+    #     controller1.get_output_port(0),
+    #     plant.get_actuation_input_port(iiwa1_model)
+    # )
 
-    builder.Connect(
-        plant.get_state_output_port(),
-        controller2.get_input_port(0)
-    )
-    builder.Connect(
-        controller2.get_output_port(0),
-        plant.get_actuation_input_port(iiwa2_model)
-    )
+    # builder.Connect(
+    #     plant.get_state_output_port(),
+    #     controller2.get_input_port(0)
+    # )
+    # builder.Connect(
+    #     controller2.get_output_port(0),
+    #     plant.get_actuation_input_port(iiwa2_model)
+    # )
 
-    # Create desired velocity sources
-    desired_velocity1 = SpatialVelocity(np.zeros(3), [0.1, 0, 0])
-    desired_velocity2 = SpatialVelocity(np.zeros(3), [0, -0.1, 0])
+    # desired_velocity1 = SpatialVelocity(np.zeros(3), [0.1, 0, 0])
+    # desired_velocity2 = SpatialVelocity(np.zeros(3), [0, -0.1, 0])
 
-    desired_velocity1_sys = builder.AddSystem(DesiredVelocitySource(desired_velocity1))
-    desired_velocity2_sys = builder.AddSystem(DesiredVelocitySource(desired_velocity2))
+    # desired_velocity1_sys = builder.AddSystem(DesiredVelocitySource(desired_velocity1))
+    # desired_velocity2_sys = builder.AddSystem(DesiredVelocitySource(desired_velocity2))
 
-    builder.Connect(
-        desired_velocity1_sys.get_output_port(0),
-        controller1.get_input_port(1)
-    )
+    # builder.Connect(
+    #     desired_velocity1_sys.get_output_port(0),
+    #     controller1.get_input_port(1)
+    # )
 
-    builder.Connect(
-        desired_velocity2_sys.get_output_port(0),
-        controller2.get_input_port(1)
-    )
+    # builder.Connect(
+    #     desired_velocity2_sys.get_output_port(0),
+    #     controller2.get_input_port(1)
+    # )
+    
     meshcat_vis.StartRecording()
     # Finalize the diagram
     full_diagram = builder.Build()
