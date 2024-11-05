7cd49b5e808cffe7dbee81bfffc20dcd01cf1768
nicogorlo <gorlon@ethz.ch>
2024-11-05 16:17:56 -0500

    removed method.log

--- src/dual_arm_manipulation/__init__.py
+++ src/dual_arm_manipulation/__init__.py
@@ -4,6 +4,13 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__fi
 
 import logging
 
+if not os.path.exists(os.path.join(ROOT_DIR, "output")):
+    os.makedirs(os.path.join(ROOT_DIR, "output"))
+
+if not os.path.exists(os.path.join(ROOT_DIR, "output/method.log")):
+    with open(os.path.join(ROOT_DIR, "output/method.log"), "w") as f:
+        f.write("")
+        
 logging.basicConfig(
     filename=os.path.join(ROOT_DIR, "output/method.log"),
     filemode="a",
